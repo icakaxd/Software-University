9c7c1fa47d7c9877d0dd8979c6824a6056b4a79d
icakaxd <icakad@icloud.com>
2020-05-30 15:28:10 +0300

    Forth problem solved without comprehensions. Fixed 80/100 with 100/100

--- Python Advanced May 2020/03. Multidumensional Lists/Lab/04.Symbol_Matrix.py
+++ Python Advanced May 2020/03. Multidumensional Lists/Lab/04.Symbol_Matrix.py
@@ -6,11 +6,17 @@ for _ in range(n):
     matrix.append(row)
 
 symbol = input()
-
+occurency = False
 for i in range(n):
+    if occurency:
+        break
     for k in range(n):
         if matrix[i][k] == symbol:
             print(f'({i}, {k})')
+            occurency = True
+
+if not occurency:
+    print(f'{symbol} does not occur in the matrix')
 
 
 """
