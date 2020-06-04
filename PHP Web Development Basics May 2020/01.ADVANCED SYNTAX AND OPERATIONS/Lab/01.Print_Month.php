bcd956df9419e0d5ad2a7baf62be8535c064729a
icakaxd <icakad@icloud.com>
2020-06-04 12:05:14 +0300

    Solved 01. Print Month

---
+++ PHP Web Development Basics May 2020/01.ADVANCED SYNTAX AND OPERATIONS/Lab/01.Print_Month.php
@@ -0,0 +1,17 @@
+<?php
+$months = array(
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+);
+
+echo $months[(int)readline() - 1];
